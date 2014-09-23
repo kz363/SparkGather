c6a8f77979a8a0996b4f321e3a0dd7e3d3ba6ba7
Kevin Zhou <kz363@nyu.edu>
2014-09-23 12:32:36 -0700

    Make controller action to parse user agent

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,19 @@
 class UsersController < ApplicationController
 
 	def gather
-		puts params.inspect
+		company = params[:c]
+		user_agent = request.env["HTTP_USER_AGENT"]
+		ip = request.remote_ip
+		browser = /(opera|chrome|safari|firefox|msie|trident)\/[^ ]*/i.match(user_agent).to_s.gsub("/", " ")
+		os = /(Mac|Windows|Linux|Android|CPU|Blackberry) \w[^;)]*/i.match(user_agent).to_s
+
+		User.create( company: company,
+								 user_agent: user_agent,
+								 ip_address: ip,
+								 browser: browser,
+								 operating_system: os )
+
+		# render whatever
 	end
 
 end
