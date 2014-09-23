d95e06c4d8f197d00a2b262568225af9c16e8a61
Kevin Zhou <kz363@nyu.edu>
2014-09-23 10:28:44 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SparkGather::Application.config.secret_key_base = '8364fdb86fce499872b3555fc542b8aedc38e390d5516b6dbfb6d0aba797d1d148a2cb0cb648f5a2d1ff61ec6cedf06c637fdb6023d2f171b4eb63fca1dfc037'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SparkGather::Application.config.session_store :cookie_store, key: '_SparkGather_session'
