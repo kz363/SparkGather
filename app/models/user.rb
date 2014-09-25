fcf06a8c3c1d2d2f4a2fd98a8a44a343210f52ca
Kevin Zhou <kz363@nyu.edu>
2014-09-25 11:46:00 -0700

    Make table for admin page that links to proper info page

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
 	def self.company_info
 		company_info = []
 		Rails.cache.read('users_info').map do |company, users|
-			@company_info << [company, users.count, users.max_by(&:updated_at)]
+			company_info << [company, users.count, users.max_by(&:updated_at).updated_at]
 		end
 		company_info.sort_by{ |info| info[0] }
 	end
--- config/initializers/cache.rb
+++ config/initializers/cache.rb
@@ -1,6 +1,12 @@
 users_info = {}
-# Hash[User.all.map { |user| [user.company, user] }]
 
-
+User.all.each do |user|
+	company = user.company
+	if users_info.keys.include?(company)
+		users_info[company] << user
+	else
+		users_info[company] = [user]
+	end
+end
 
 Rails.cache.write('users_info', users_info)
