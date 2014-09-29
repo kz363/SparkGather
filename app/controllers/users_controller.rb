e7c71f8aa91526870e58bf7b3cbf15f65635e663
tihuan <TimmyHuang0404@Gmail.com>
2014-09-29 16:15:47 -0700

    change #index back to just @company_info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,7 @@ class UsersController < ApplicationController
 	MOBILE_DEVICES = /(iPhone|iPod|iPad|Android|Phone|IEMobile|Tablet|Mobile|Nokia)/
 
 	def index
-		page_number = params[:page].to_i - 1
-		@company_info = User.company_info[page_number]
+		@company_info = User.company_info
 	end
 
 	def gather
@@ -38,7 +37,7 @@ class UsersController < ApplicationController
 		@user = User.find(params[:id])
 	end
 
-	private
+private
 
 	def mobile_browser?
 		request.env['HTTP_USER_AGENT'] && request.env["HTTP_USER_AGENT"][MOBILE_DEVICES] ? true : false
