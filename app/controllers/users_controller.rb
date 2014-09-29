9320aa28b96388acb13f0ab579390780f9ce9d02
Kevin Zhou <kz363@nyu.edu>
2014-09-29 15:21:29 -0700

    Add cookie to prevent multiple entries from the same user within 7 days

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,25 +2,30 @@ class UsersController < ApplicationController
 	MOBILE_DEVICES = /(iPhone|iPod|iPad|Android|Phone|IEMobile|Tablet|Mobile|Nokia)/
 
 	def index
-		puts Cache.methods.sort
 		@company_info = User.company_info
 	end
 
 	def gather
-		redirect_to info_error_path if params[:c] == "" || params[:c] == nil
-		company = params[:c]
-		user_agent = request.env["HTTP_USER_AGENT"]
-		ip_address = request.remote_ip
-		browser = parse_browser(user_agent)
-		operating_system = parse_operating_system(user_agent)
-		proxy = Proxy.check_proxy(ip_address)
-		@user = User.create(company: company,
-												user_agent: user_agent,
-												ip_address: ip_address,
-												browser: browser,
-												operating_system: operating_system,
-												proxy: proxy,
-												mobile: mobile_browser?)
+		if params[:c] == "" || params[:c] == nil
+			redirect_to info_error_path
+		elsif cookies[:guidespark_id]
+			@user = User.find(cookies.signed[:guidespark_id])
+		else
+			company = params[:c]
+			user_agent = request.env["HTTP_USER_AGENT"]
+			ip_address = request.remote_ip
+			browser = parse_browser(user_agent)
+			operating_system = parse_operating_system(user_agent)
+			proxy = Proxy.check_proxy(ip_address)
+			@user = User.create(company: company,
+													user_agent: user_agent,
+													ip_address: ip_address,
+													browser: browser,
+													operating_system: operating_system,
+													proxy: proxy,
+													mobile: mobile_browser?)
+			cookies.signed[:guidespark_id] = { value: @user.id, expires: 7.days.from_now }
+		end
 	end
 
 	def update
