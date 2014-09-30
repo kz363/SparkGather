b67b02d1b4eabb1c239b1bb9ca68c3436a4d4b80
tihuan <TimmyHuang0404@Gmail.com>
2014-09-30 08:58:05 -0700

    update collection to 10 records per page

--- app/presenters/users/company_presenter.rb
+++ app/presenters/users/company_presenter.rb
@@ -1,4 +1,5 @@
 class Users::CompanyPresenter
+	RECORDS_PER_PAGE = 10
 	attr_reader :name
 
 	def initialize(name)
@@ -6,7 +7,7 @@ class Users::CompanyPresenter
 	end
 
 	def users
-		Rails.cache.read('users_info')[name].sort { |a,b| b.id <=> a.id }
+		Rails.cache.read('users_info')[name].sort { |a,b| b.id <=> a.id }.each_slice(RECORDS_PER_PAGE).to_a
 	end
 
 	def metadata
