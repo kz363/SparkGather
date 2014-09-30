8fb96ee6bd38aa614344c2c4b4d9c89a03e95817
tihuan <TimmyHuang0404@Gmail.com>
2014-09-29 20:01:00 -0700

    add helper methods to construct _pagination

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -11,11 +11,32 @@ module UsersHelper
 		)
 	end
 
+	def page_number
+		current_page - 1
+	end
+
+	def arrow_left
+		handle_end_page(0, '&laquo;', '-')
+	end
+
+	def arrow_right(companies)
+		handle_end_page(companies.size - 1, '&raquo;', '+')
+	end
+
+private
+
 	def current_page
 		params[:page].to_i
 	end
 
-	def page_number
-		current_page - 1
+	def handle_end_page(end_page, arrow, direciton)
+		disabled = page_number == end_page ? "disabled" : nil
+		content_tag :li, class: disabled do
+			if disabled
+				content_tag(:span, arrow.html_safe).html_safe
+			else
+				content_tag(:a, arrow.html_safe, href: "/?page=#{eval(current_page.to_s + "#{direciton}" + ' 1')}").html_safe
+			end
+		end
 	end
 end
