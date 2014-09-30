24bfad0b8fda84bbd2e48a19c267568ac78cecb1
tihuan <TimmyHuang0404@Gmail.com>
2014-09-30 15:15:40 -0700

    create link form

--- app/models/link_form.rb
+++ app/models/link_form.rb
@@ -2,6 +2,16 @@ class LinkForm
   include ActiveModel::Model
   include ActiveModel::Validations
 
-  attr_accessor :company_name, :
+  attr_accessor :company_name, :salesforce
+  validates_presence_of :company_name
 
+  def initialize(attributes = {})
+    attributes.each do |name, value|
+      send("#{name}=", value)
+    end
+  end
+
+  def persisted?
+    false
+  end
 end
